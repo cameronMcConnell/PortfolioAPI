46d830d6419fa40f7090f23477b64d4360e99031
Cameron McConnell <cameron@angry-dogman.com>
2024-08-04 19:04:18 -0500

    Updated deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-# Build the container
-docker build -t portfolio-api .
-
 # Get site from repository
 cp -r ../Portfolio/site .
+
+# Build the container
+docker build -t portfolio-api .
 
 # Run the container
 docker run -d -p 80:80 portfolio-api
